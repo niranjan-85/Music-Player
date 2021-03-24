a4747402c9299e5c348948435c58232da3ae0dfe
Niranjan Raje <raje.niranjan18@siesgst.ac.in>
2021-03-25 01:04:14 +0530

    User Registration added

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const bodyparser = require('body-parser');
-
+const mongoose = require('mongoose');
 const router = require('./src/routes/routes')
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 5000 ;
 
 // static files 
@@ -16,9 +15,19 @@ app.use('/images',express.static(__dirname + 'public/images'))
 app.set('view engine','ejs')
 app.set('views','./src/views')
 
+//bodyparser 
+app.use(express.urlencoded({ extended: false }));
+
+//Database config 
+const mongoURI = 'mongodb://localhost:27017/musicplayer';
+mongoose.connect(mongoURI,{useNewUrlParser:true,useUnifiedTopology: true })
+    .then(()=>{console.log("DB connected")})
+    .catch((err)=>{console.log("error")});
+
 //Routes 
 
 app.get('/',router)
+app.post('/users',router)
 
 
 // Server 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,48 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const UserModel = require('../../models/user')
 const Router = express.Router();
 
 // Handiling Routes :-
 
+// homepage
 Router.get('/',(req,res)=>{
-    res.render('index.ejs');
+    res.render('index.ejs',{is_error:0});
 })
+
+// handle Register 
+Router.post('/users',async (req,res)=>{
+    let {name,Username,password } = req.body
+    User = await UserModel.findOne({Username : Username});
+    if(User){
+        res.render('index.ejs',{is_error:1,error_msg:"User Already Exists"});
+    }
+    else{
+        //Create a new user model
+
+        const newUser = new UserModel({
+            name,
+            Username,
+            password
+        });
+
+        // Hashing password :
+
+        bcrypt.genSalt(10,async (err,salt)=>{
+             bcrypt.hash(newUser.password,salt,(error,hash)=>{
+                if(error) throw error;
+                newUser.password=hash
+
+                // saving to db
+                newUser.save()
+                    .then(()=>{console.log("user added to db")})
+                    .catch((error)=>{console.log("Error while updating db")})
+            })
+        });
+
+    }
+
+
+});
 
 module.exports = Router;
